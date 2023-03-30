d4db9e8c9083112aedfd87e0febb581c91c3304c
Ren Blake <rcblake06@gmail.com>
2023-03-30 13:47:27 -0700

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+main.remove()
+
+const newHeader = document.createElement("h1") 
+
+newHeader.id = "victory"
+
+newHeader.innerHTML = "Ren Blake is the champion"
+
+document.body.append(newHeader)
